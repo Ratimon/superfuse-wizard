bf6f040a609c2571df3efffc7dbc75cda22887f5
Rati Montreewat <ratixoxo@gmail.com>
2024-12-18 16:43:10 +0700

    Build (./): Using postcss and modifying theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,64 @@
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import daisyui from "daisyui"
-import type { Config } from 'tailwindcss';
+const daisyui = require('daisyui');
+const typography = require('@tailwindcss/typography');
+const forms = require('@tailwindcss/forms')({ strategy: 'class' });
 
-export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        gradient:
+          "linear-gradient(60deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82)",
+      },
+      animation: {
+        opacity: "opacity 0.25s ease-in-out",
+        appearFromRight: "appearFromRight 300ms ease-in-out",
+        wiggle: "wiggle 1.5s ease-in-out infinite",
+        popup: "popup 0.25s ease-in-out",
+        shimmer: "shimmer 3s ease-out infinite alternate",
+      },
+      keyframes: {
+        opacity: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        appearFromRight: {
+          "0%": { opacity: 0.3, transform: "translate(15%, 0px);" },
+          "100%": { opacity: 1, transform: "translate(0);" },
+        },
+        wiggle: {
+          "0%, 20%, 80%, 100%": {
+            transform: "rotate(0deg)",
+          },
+          "30%, 60%": {
+            transform: "rotate(-2deg)",
+          },
+          "40%, 70%": {
+            transform: "rotate(2deg)",
+          },
+          "45%": {
+            transform: "rotate(-4deg)",
+          },
+          "55%": {
+            transform: "rotate(4deg)",
+          },
+        },
+        popup: {
+          "0%": { transform: "scale(0.8)", opacity: 0.8 },
+          "50%": { transform: "scale(1.1)", opacity: 1 },
+          "100%": { transform: "scale(1)", opacity: 1 },
+        },
+        shimmer: {
+          "0%": { backgroundPosition: "0 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+          "100%": { backgroundPosition: "0% 50%" },
+        },
+      },
+    },
   },
 
-  plugins: [forms, typography, daisyui],
+	plugins: [forms, typography, daisyui],
 	daisyui: {
 		logs: false,
 		// darkTheme: 'dracula',
@@ -24,13 +72,13 @@ export default {
 			// 'synthwave',
 			// 'retro',
 			// 'cyberpunk',
+			// 'valentine',
 			{
 				cyberpunk: {
 				  ...require("daisyui/src/theming/themes")["cyberpunk"],
 				  "base-100": "#E0A3B7",
 				},
 			},
-			// 'valentine',
 			// 'halloween',
 			// 'garden',
 			// 'forest',
@@ -52,4 +100,6 @@ export default {
 			// 'winter',
 		],
 	},
-} satisfies Config;
+};
+
+module.exports = config;
